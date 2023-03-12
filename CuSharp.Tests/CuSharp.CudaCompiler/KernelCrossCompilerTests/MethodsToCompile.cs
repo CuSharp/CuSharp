4279bb972551ae07829d18e7ce2b438e38defa8e
Jason Benz <j.benz@webbenz.ch>
2023-03-12 20:55:34 +0100

    Add MethodBodyCompiler tests

---
+++ CuSharp.Tests/CuSharp.CudaCompiler/KernelCrossCompilerTests/MethodsToCompile.cs
@@ -0,0 +1,48 @@
+﻿namespace CuSharp.Tests.CuSharp.CudaCompiler.KernelCrossCompilerTests
+{
+    public class MethodsToCompile
+    {
+        public void ScalarIntAddition(int a, int b)
+        {
+            int c = a + b;
+        }
+
+        public void ScalarIntSubtraction(int a, int b)
+        {
+            int c = a - b;
+        }
+
+        public void ScalarIntMultiplication(int a, int b)
+        {
+            int c = a * b;
+        }
+
+        public void ScalarFloatAddition(float a, float b)
+        {
+            float c = a + b;
+        }
+
+        public void ScalarFloatSubtraction(float a, float b)
+        {
+            float c = a - b;
+        }
+
+        public void ScalarFloatMultiplication(float a, float b)
+        {
+            float c = a * b;
+        }
+
+
+        public void ArrayIntAddition(int[] a, int[] b, int[] c)
+        {
+            int i = 0;
+            c[i] = a[i] + b[i];
+        }
+
+        public void ArrayFloatAddition(float[] a, float[] b, float[] c)
+        {
+            int i = 0;
+            c[i] = a[i] + b[i];
+        }
+    }
+}
