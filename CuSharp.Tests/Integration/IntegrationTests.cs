7689946a4da05c361b6b176c502302217b8a27bd
Jason Benz <j.benz@webbenz.ch>
2023-03-17 22:06:45 +0100

    Update integration test

--- CuSharp.Tests/Integration/IntegrationTests.cs
+++ CuSharp.Tests/Integration/IntegrationTests.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
+﻿using System.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CuSharp.Tests.Integration;
 
 public class IntegrationTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public IntegrationTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     void SimpleArrayAdd(int[] a, int[] b, int[] c)
     {
         var idx = KernelTools.BlockDimensions.Item1 * KernelTools.BlockIndex.Item1 + KernelTools.ThreadIndex.Item1;
@@ -17,7 +23,6 @@ public class IntegrationTests
     public void TestSimpleArrayAdd()
     {
         var dev = global::CuSharp.CuSharp.GetDefaultDevice();
-        var devName = dev.ToString();
         int[] a = new[] {1, 2, 3};
         int[] b = new[] {4, 5, 6};
         int[] c = new int[3];
@@ -25,11 +30,11 @@ public class IntegrationTests
         var devB = dev.Copy(b);
         var devC = dev.Copy(c);
         global::CuSharp.CuSharp.StartTimer();
-        int j = 0;
 
         dev.Launch(SimpleArrayAdd, (1,1,1), (3,1,1), devA, devB, devC);
         float time = global::CuSharp.CuSharp.GetTimeMS();
         c = dev.Copy(devC);
+        _output.WriteLine($"Calculation time on gpu device '{dev.ToString()}': {time}ms");
         Assert.True(c.SequenceEqual(new []{5,7,9}));
     }
 }
