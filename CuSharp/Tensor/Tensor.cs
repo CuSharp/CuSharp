57daa18cb2f41f4e4fdb7026ad01a17dfd9f7c84
LoAdrian <locher-adrian@hotmail.com>
2023-05-05 18:43:48 +0200

    remove example code

--- CuSharp/Tensor/Tensor.cs
+++ CuSharp/Tensor/Tensor.cs
@@ -7,34 +7,9 @@ public abstract class Tensor<T> : IDisposable
 { /*Meant as an opaque Handle*/
 
     //implicit conversion for scalar values
-    public int fib(int n)
-    {
-        if (n < 3)
-        {
-            return 1;
-        }
-
-        return fib(n - 1) + fib(n - 2);
-    }
-
-    public int fib2(int n)
-    {
-        int x = 0;
-        int y = 1;
-        int z = 0;
-        for (int i = 0; i < n; i++)
-        {
-            z = x + y;
-            x = y;
-            y = z;
-        }
-
-        return z;
-    }
-    
     public static implicit operator Tensor<T>(T value)
     {
-            return new CudaScalar<T>(value);
+        return new CudaScalar<T>(value);
         if (typeof(T).IsArray)
         {
         }
