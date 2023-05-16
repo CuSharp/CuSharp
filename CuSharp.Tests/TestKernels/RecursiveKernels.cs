461b17520ff42d99958fa8a2c92e69b24be8f801
LoAdrian <locher-adrian@hotmail.com>
2023-05-16 15:11:23 +0200

    add recursion test kernels

---
+++ CuSharp.Tests/TestKernels/RecursiveKernels.cs
@@ -0,0 +1,22 @@
+﻿using CuSharp.Kernel;
+
+namespace CuSharp.Tests.TestHelper;
+
+public class RecursiveKernels
+{
+    [Kernel]
+    public static void FibonacciLauncher(int[] results, int n)
+    {
+        Fibonacci(results, n);
+    }
+    public static int Fibonacci(int[] results, int n) {
+        if (n <= 2)
+        {
+            results[n] = 1;
+            return 1;
+        }
+        results[n-1] = Fibonacci(results, n-1) + Fibonacci(results, n-2);
+        return results[n - 1];
+    }
+    
+}
