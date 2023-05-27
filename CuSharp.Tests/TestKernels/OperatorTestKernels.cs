ecbef4c47f96c47b81525ccd614ea681328875e2
Jason Benz <j.benz@webbenz.ch>
2023-05-27 10:08:14 +0200

    Implement rem_un and div_un

--- CuSharp.Tests/TestKernels/OperatorTestKernels.cs
+++ CuSharp.Tests/TestKernels/OperatorTestKernels.cs
@@ -1,4 +1,4 @@
-﻿namespace CuSharp.Tests.TestHelper;
+﻿namespace CuSharp.Tests.TestKernels;
 
 public class OperatorTestKernels
 {
@@ -6,22 +6,37 @@ public class OperatorTestKernels
     {
         a[0] += b[0];
     }
+
     public static void FloatSubtraction(float[] a, float[] b)
     {
         a[0] -= b[0];
     }
+
     public static void FloatMultiplication(float[] a, float[] b)
     {
         a[0] *= b[0];
     }
+
+    public static void UintDivision(uint a, uint b, uint[] result)
+    {
+        result[0] = a / b;
+    }
+
     public static void FloatDivision(float[] a, float[] b)
     {
         a[0] /= b[0];
     }
+
+    public static void UintModulo(uint a, uint b, uint[] result)
+    {
+        result[0] = a % b;
+    }
+
     public static void FloatModulo(float[] a, float[] b)
     {
         a[0] %= b[0];
     }
+
     public static void NotTest(bool[] a, bool b)
     {
         a[0] = b;
