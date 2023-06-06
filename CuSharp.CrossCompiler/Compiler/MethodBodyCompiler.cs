0a558ca0a1b6b7fb88bfe1a7df5ff4e1d2f4f31f
LoAdrian <locher-adrian@hotmail.com>
2023-06-06 16:07:54 +0200

    update

--- CuSharp.CrossCompiler/Compiler/MethodBodyCompiler.cs
+++ CuSharp.CrossCompiler/Compiler/MethodBodyCompiler.cs
@@ -897,17 +897,15 @@ public class MethodBodyCompiler
     private void CompileBrFalse(int operand)
     {
         var predicate = _cfg.CurrentBlock.VirtualRegisterStack.Pop();
-        LLVMValueRef predicateNegated;
-        
+
         if (!predicate.TypeOf().Equals(LLVMTypeRef.Int1Type())) //converting to bool (i1)
         {
-            predicateNegated = LLVM.BuildICmp(_builder, LLVMIntPredicate.LLVMIntNE, predicate,
-                LLVM.ConstInt(predicate.TypeOf(), 1, false), GetVirtualRegisterName());
+            predicate = LLVM.BuildICmp(_builder, LLVMIntPredicate.LLVMIntNE, predicate,
+                LLVM.ConstInt(predicate.TypeOf(), 0, false), GetVirtualRegisterName());
         }
-        else
-        {
-            predicateNegated = LLVM.BuildNot(_builder, predicate, GetVirtualRegisterName());
-        }
+        
+        var predicateNegated = LLVM.BuildNot(_builder, predicate, GetVirtualRegisterName());
+        
         BuildConditionalBranch(_stream.Position + operand, _stream.Position, predicateNegated);
     }
 
