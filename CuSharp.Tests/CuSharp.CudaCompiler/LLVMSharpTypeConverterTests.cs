f1f2d1116b537e888ccc89931c086a5c14f82078
Jason Benz <j.benz@webbenz.ch>
2023-06-13 14:54:08 +0200

    Add type tests

--- CuSharp.Tests/CuSharp.CudaCompiler/LLVMSharpTypeConverterTests.cs
+++ CuSharp.Tests/CuSharp.CudaCompiler/LLVMSharpTypeConverterTests.cs
@@ -18,10 +18,22 @@ public class LLVMSharpTypeConverterTests
     private bool TestLLVMToNative(LLVMTypeRef llvmType, Type nativeType)
     {
         return llvmType.ToNativeType() == nativeType;
-    } 
+    }
+
+    [Fact]
+    public void TestVoidToLLVMVoid()
+    {
+        Assert.True(TestNativeToLLVM(LLVMTypeRef.VoidType(), typeof(void)));
+    }
+
+    [Fact]
+    public void TestLLVMVoidToVoid()
+    {
+        Assert.True(TestLLVMToNative(LLVMTypeRef.VoidType(), typeof(void)));
+    }
     
     [Fact]
-    public void TestBoolToLLVMBoolSuccess()
+    public void TestBoolToLLVMBool()
     {
         Assert.True(TestNativeToLLVM(LLVMTypeRef.Int1Type(), typeof(bool)));
     }
@@ -102,6 +114,24 @@ public class LLVMSharpTypeConverterTests
     public void TestLLVMDoubleToDouble()
     {
         Assert.True(TestLLVMToNative(LLVMTypeRef.DoubleType(), typeof(double)));
+    }
+
+    [Fact]
+    public void TestUintToLLVMInt()
+    {
+        Assert.True(TestNativeToLLVM(LLVMTypeRef.Int32Type(), typeof(uint)));
+    }
+
+    [Fact]
+    public void TestArrayToLLVMPointerType()
+    {
+        Assert.True(TestNativeToLLVM(LLVMTypeRef.PointerType(LLVMTypeRef.Int32Type(), 0), typeof(int[])));
+    }
+
+    [Fact]
+    public void TestLLVMPointerTypeToArray()
+    {
+        Assert.True(TestLLVMToNative(LLVMTypeRef.PointerType(LLVMTypeRef.Int32Type(), 0), typeof(int[])));
     }
 
     [Fact]
